a351d237afb5a1d53264941ec7d499f45ea0db39
dewi22222222 <dewiksp972@gmail.com>
2024-08-27 16:35:15 +0700

    tipe data integer di php

---
+++ 19.tipe data integer di php.php
@@ -0,0 +1,9 @@
+<?php
+
+$nilai = 80; // angka positif
+$poin = -27; // angka negative
+
+echo "angka positif: $nilai";
+echo "angka negatif: $poin";
+
+echo "<p><strong>By dewi puspita sari</strong>";
