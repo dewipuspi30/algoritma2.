63c51165afceea8686e312f859b497ef414d15c6
dewi22222222 <dewiksp972@gmail.com>
2024-08-27 17:09:09 +0700

    tipe data float di php.php

--- 20.tipe data float di php.php
+++ 20.tipe data float di php.php
@@ -1,10 +1,11 @@
 <?php
 
 $waktu = 230.12;
-$kecepatan = 13235.12;
-$jarak = 230.12; * 13235.12;
+$kecepatan = 13235.12 ;
+$jarak = 230.12 * 13235.12;
 
 // batasi angka di belakang koma
 echo "<p>nilai jarak: $jarak";
 
 echo "<p><strong>By dewi puspita sari</strong>";
+
