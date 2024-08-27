6cfe0b950f458307a97ca6af435377e86a3b62c7
dewi22222222 <dewiksp972@gmail.com>
2024-08-27 15:45:39 +0700

    fungsi print.php

---
+++ 13.fungsi print.php
@@ -0,0 +1,7 @@
+<?php
+print "<h2>Belajar PHP dari Nol!</h2>";
+print "Hello dewi puspita sari!<br>";
+print "Belajar mecetak teks di PHP!";
+?>
+
+echo "<br><br><br><strong>By dewi puspita sari</strong>";
