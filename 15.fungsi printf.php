4e334166c75c2ea88ae53b49a12c799d701a7856
dewi22222222 <dewiksp972@gmail.com>
2024-08-27 15:57:05 +0700

    fungsi printf.php

---
+++ 15.fungsi printf.php
@@ -0,0 +1,12 @@
+<?php
+// misalkan kita punya bilangan dengan notasi E seperti ini
+$harga = 400000;
+
+// jika kita cetak dengan echo:
+echo "Harganya adalah Rp $harga";
+
+// jika kita cetak dengan printf
+printf("Harganya adalah Rp %.2f", $harga);
+
+
+echo "<br><br><br><strong>By dewi puspita sari</strong>";
