4aa0b2b865caaacd3980ec48f5c7b2b94dec3a75
dewi22222222 <dewiksp972@gmail.com>
2024-08-27 17:03:11 +0700

    tipe data float di php.php

--- 20.tipe data float di php.php
+++ 20.tipe data float di php.php
@@ -1,3 +1,10 @@
-echo sprintf('%f', $a);
+<?php
+
+$waktu = 230.12;
+$kecepatan = 13235.12;
+$jarak = 230.12; * 13235.12;
+
 // batasi angka di belakang koma
-echo sprintf('%.3f', $a);
+echo "<p>nilai jarak: $jarak";
+
+echo "<p><strong>By dewi puspita sari</strong>";
