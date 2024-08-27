d2eb160ac945945f6c40052a1e071613a5ad663d
dewi22222222 <dewiksp972@gmail.com>
2024-08-27 11:32:09 +0700

    aturan penulisan case php.php

---
+++ 5.aturan penulisan case php .php
@@ -0,0 +1,16 @@
+<?php
+
+$nama = "dewi puspita sari";
+$NAMA ="dewi";
+$umur =16;
+const INI_KONSTANTA =343;
+$ini_variabel =30;
+$ini_variabel_variabel =35;
+$ini_variabel ;
+
+echo "{======= mencetak variabel =======}";
+echo "<p>Nama saya adalah $nama";
+echo "<p>Nama panggilan saya adalah $NAMA";
+echo "<p>Umur saya adalah $umur";
+
+echo "br><br><br><strong>by dewi puspita sari</strong>";
