faf852d83028053037919e88985c221f83bdd0e9
dewi22222222 <dewiksp972@gmail.com>
2024-08-28 07:37:50 +0700

    tipe data bolean di php.di php

---
+++ 21.tipe data bolean di php.php
@@ -0,0 +1,12 @@
+<?php
+//mendeklarasi variabel boolean
+$isActive = false;
+$menikah = true;
+//menggunakan boolean dalam pernyataan if
+if ($isActive){
+    echo "nilai ini benar.";
+} else{
+    echo "nilai ini salah.";
+}
+
+echo "<p><strong>By dewi puspita sari</strong>";
