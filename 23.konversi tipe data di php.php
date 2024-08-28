20aea5fcc836b25ce766487012aca491c9aa9622
dewi22222222 <dewiksp972@gmail.com>
2024-08-28 08:36:43 +0700

    konversi tipe data di php.php

---
+++ 23.konversi tipe data di php.php
@@ -0,0 +1,10 @@
+<?php
+$var = "234.56";
+$intrva = "234";
+$intvar = intval($var);//mengubah menjadi integer
+$floatvar = floatval($var);//mengubah menjadu float
+echo $intvar;//output:234
+echo "<p>$floatvar";//output:234.56
+
+echo "<P><br><strong>by dewi puspita sari </strong>";
+?>
